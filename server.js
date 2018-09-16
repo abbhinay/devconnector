b7c838ad07c00aabb5a85bcdf618ccb815c05899
Abhinay Kashyap <abhi01.amu@gmail.com>
2018-09-16 11:58:13 -0500

    after 10 User Registration

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -9,6 +10,10 @@ const posts = require('./routes/api/posts');
 // MongoClient.connect({ useNewUrlParser: true });
 
 const app = express();
+
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB Config
 const db = require('./config/keys').mongoURI;
